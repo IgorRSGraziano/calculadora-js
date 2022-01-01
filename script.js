168ef95d9979bf085b7edfa8d9ae007919e12786
IgorRSGraziano <igorrsgraziano@gmail.com>
2022-01-01 18:26:33 -0300

    add main functions

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 console.log("Hello World <3")
+
+
+function insert(number) {
+    let result = document.querySelector(".calculator-result").textContent;
+    document.querySelector(".calculator-result").textContent = result + number
+
+}
+
+function clean() {
+    document.querySelector(".calculator-result").textContent = ''
+}
+
+function backspace() {
+    let result = document.querySelector(".calculator-result").textContent
+    document.querySelector(".calculator-result").textContent = result.slice(0, -1);
+}
+
+function calculate() {
+    let result = document.querySelector(".calculator-result").textContent
+    document.querySelector(".calculator-result").textContent = eval(result)
+}
